d4a36ddaf536ef5513c6c37e1fe7fadd10631719
denverku <magicph26@gmail.com>
2023-10-20 18:27:31 +0800

    Update license.js

--- app/script/license.js
+++ app/script/license.js
@@ -1,1 +1,12 @@
+const config = {
+    apiKey: "AIzaSyCKCBMCTbjg-Wazz_WhCYtxbmM0qsUHKsg",
+    authDomain: "XXXXX",
+    databaseURL: "https://vpnconfig-70581-default-rtdb.firebaseio.com",
+    projectId: "vpnconfig-70581",
+    storageBucket: "tweetgo-main.appspot.com",
+    messagingSenderId: "XXXXXX"
+}
 
+var defaultApp = firebase.initializeApp(config);
+console.log(defaultApp.name);
+firebase.auth().signInWithEmailAndPassword("sidor@iappstore.cf", "trunks123");
